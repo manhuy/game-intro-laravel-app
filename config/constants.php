12c5bfc885db145b1bf378373068c186b215376d
MANHUY\nlhuy <nlhuy1997@gmail.com>
2023-02-08 00:33:23 +0700

    add ui auth

---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php 
+
+// Role definitions
+define('NORMAL_USER_ROLE', 0);
+define('ADMIN_ROLE', 1);
+
+?>
---
+++ database/migrations/2023_02_07_162734_add_active_and_role_column_into_users_talbe.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->tinyInteger('is_active')->default(0)->after('password')->comment('0: inactive; 1: active');
+            $table->tinyInteger('role')->default(0)->after('is_active')->comment('0: normal user; 1: admin');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn(['is_active', 'role']);
+        });
+    }
+};
